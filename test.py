92b7b43f637c4b887cc218e54448b0300ca17faa
Cz108 <liualvin98@gmail.com>
2023-07-27 22:14:22 +0200

    language detection test

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-#Test only
+# Test only
+import os
+from tools import snippets as tool
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/bilibala/Study/SLA-TRANS/cre/sla.json"
+
+
+
+print(tool.detect_language("你好吗？"))
+print(tool.detect_language("Dzien dobry？"))
+print(tool.detect_language("это за день на това?"))
